470b9ee8ad662fe1e1390317e3b4373747246313
Igor Anic <ianic@inet.hr>
2022-08-11 17:09:53 +0200

    cleanup board code

--- src/modules/boards/nucleo_stm32f411/nucleo_stm32f411.zig
+++ src/modules/boards/nucleo_stm32f411/nucleo_stm32f411.zig
@@ -2,33 +2,14 @@ const std = @import("std");
 pub const micro = @import("microzig");
 pub const chip = micro.chip;
 pub const regs = chip.registers;
-pub const clock = @import("clock.zig");
 pub const irq = chip.irq;
 
-// // HSI defaults after restart
-// const cpu_clock = 84_000_000;
-// pub const clock_frequencies = .{
-//     .cpu = cpu_clock,
-//     .ahb = cpu_clock / 1,
-//     .apb1 = cpu_clock / 2,
-//     .apb2 = cpu_clock / 1,
-// };
-
 pub const Config = struct {
-    //clock: clock.Config = clock.hsi_high,
     led_enabled: bool = true,
     key_enabled: bool = true,
-    //systick_enabled: bool = true,
 };
 
 pub fn init(cfg: Config) void {
-    //initFeatures();
-    // TODO: clock spada u chip, ovdje ostaju hsi i hse frekvencije
-    // zapravo samo hse
-    //clock.init(cfg.clock);
-    // if (cfg.systick_enabled) {
-    //     initSysTick(1);
-    // }
     if (cfg.led_enabled) {
         led.init();
         //  TODO high speed opcija za gpio
@@ -40,11 +21,11 @@ pub fn init(cfg: Config) void {
 }
 
 pub const pin_map = .{
-    .@"LD2" = "PA5", // on-board user led
-    .@"KEY1" = "PC13", // on-board user key
+    .led = "PA5", // on-board user led
+    .button = "PC13", // on-board user key
 };
 
-pub const led = micro.Gpio(micro.Pin(pin_map.LD2), .{
+pub const led = micro.Gpio(micro.Pin(pin_map.led), .{
     .mode = .output,
     .initial_state = .high,
 });
@@ -52,7 +33,7 @@ pub const led = micro.Gpio(micro.Pin(pin_map.LD2), .{
 fn keyInit() void {
     regs.RCC.APB2ENR.modify(.{ .SYSCFGEN = 1 }); // Enable SYSCFG Clock
     // init key as input
-    micro.Gpio(micro.Pin(pin_map.KEY1), .{ .mode = .input }).init();
+    micro.Gpio(micro.Pin(pin_map.button), .{ .mode = .input }).init();
     // above micro is same as below regs two lines
     // // PC13 enable input
     // regs.RCC.AHB1ENR.modify(.{ .GPIOCEN = 1 });
@@ -67,34 +48,35 @@ fn keyInit() void {
     irq.enable(.exti15_10);
 }
 
-// fn initFeatures() void {
-//     regs.FLASH.ACR.modify(.{ .DCEN = 1, .ICEN = 1, .PRFTEN = 1 }); // Enable flash data and instruction cache
-//     regs.SCB.AIRCR.modify(.{ .PRIGROUP = 0b011, .VECTKEYSTAT = 0x5FA }); // Set Interrupt Group Priority
-//     regs.FPU_CPACR.CPACR.modify(.{ .CP = 0b1111 }); // Enable FPU coprocessor
-// }
+// this board uses crystal oscilator from st-link part of the board for hse
+pub const hse_frequency = 8_000_000; // 8 MHz
 
-// // init SysTick interrupt every ms milliseconds
-// fn initSysTick(ms: u16) void {
-//     const reload: u24 = @intCast(u24, clock.frequencies.ahb / 8) / 1000 * ms - 1;
-//     //const reload: u24 = @intCast(u24, clock.frequencies.ahb / 1000) * ms - 1;  // for clocksource = 1
-//     regs.SCS.SysTick.LOAD.modify(.{ .RELOAD = reload }); // set counter
-//     regs.SCB.SHPR3.modify(.{ .PRI_15 = 0xf0 }); // SysTick IRQ priority
-//     regs.SCS.SysTick.VAL.modify(.{ .CURRENT = 0 }); // start from 0
-//     // CLKSOURCE:
-//     //   0: AHB/8
-//     //   1: Processor clock (AHB)
-//     // TICKINT:
-//     //   0: Counting down to zero does not assert the SysTick exception request
-//     //   1: Counting down to zero to asserts the SysTick exception request.
-//     //
-//     regs.SCS.SysTick.CTRL.modify(.{
-//         .ENABLE = 1,
-//         .CLKSOURCE = 0,
-//         .TICKINT = 1,
-//     });
-// }
-
-pub const hse_frequency = 8_000_000; // 8MHz
+// To use hse put something like this in app init:
+//    const micro = @import("microzig");
+//    const chip = micro.chip;
+//    const board = micro.board;
+//    ...
+//    const ccfg: chip.Config = .{ .clock = board.hse_96 };
+//    chip.init(ccfg);
+//
+pub const hse_100 = .{
+    .source = .hse,
+    .m = 2,
+    .n = 100,
+    .p = 4,
+    .q = 8,
+    .latency = 3,
+    .ahb_prescaler = 1,
+    .apb1_prescaler = 2,
+    .apb2_prescaler = 1,
+    .frequencies = .{
+        .source = hse_frequency,
+        .cpu = 100_000_000,
+        .ahb = 100_000_000,
+        .apb1 = 50_000_000,
+        .apb2 = 100_000_000,
+    },
+};
 
 pub const hse_96 = .{
     .source = .hse,
@@ -112,24 +94,5 @@ pub const hse_96 = .{
         .ahb = 96_000_000,
         .apb1 = 48_000_000,
         .apb2 = 96_000_000,
-    },
-};
-
-pub const hse_100 = .{
-    .source = .hse,
-    .m = 2,
-    .n = 100,
-    .p = 4,
-    .q = 8,
-    .latency = 3,
-    .ahb_prescaler = 1,
-    .apb1_prescaler = 2,
-    .apb2_prescaler = 1,
-    .frequencies = .{
-        .source = hse_frequency,
-        .cpu = 100_000_000,
-        .ahb = 100_000_000,
-        .apb1 = 50_000_000,
-        .apb2 = 100_000_000,
     },
 };
